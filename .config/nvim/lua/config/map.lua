bd3fea9503168fd933620b76f73bc5ddc7d2fc39
Kamal Marhubi <kamal@marhubi.com>
2020-10-31 21:10:51 -0400

    Add <leader>vf to navigate vim config

--- .config/nvim/lua/config/map.lua
+++ .config/nvim/lua/config/map.lua
@@ -1,7 +1,11 @@
 vim.api.nvim_set_keymap('', '<space>', '<leader>', { silent = true })
 
 -- <leader>v: vim stuff
+-- reload
 vim.api.nvim_set_keymap('n', '<leader>vr', [[<cmd>lua require('config.util').reload()<CR>]], { noremap = true })
+-- open files
+vim.api.nvim_set_keymap('n', '<leader>vf', [[<cmd>lua require('telescope.builtin').find_files{ cwd = "~/.config/nvim/" }<CR>]], { noremap = true })
 
 -- <leader>n: navigation stuff
+-- open files
 vim.api.nvim_set_keymap('n', '<leader>nf', [[<cmd>lua require('telescope.builtin').git_files{}<CR>]], { noremap = true })
