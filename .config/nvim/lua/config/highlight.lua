9e679eeb6bd16e8141704f827322d7137207638b
Kamal Marhubi <kamal@marhubi.com>
2020-11-01 18:47:08 -0500

    Use treesitter for highlihting

--- .config/nvim/lua/config/highlight.lua
+++ .config/nvim/lua/config/highlight.lua
@@ -1,2 +1,6 @@
--- TODO: fix this
+-- We're going to rely on treesitter for syntax and see how that goes. I
+-- *think* this means I can leave syntax disabled.
 vim.cmd('syntax off')
+
+-- We have lots of colours!
+vim.o.termguicolors = true
--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -2,3 +2,16 @@ require('config.abbr')
 require('config.highlight')
 require('config.map')
 require('config.opt')
+
+
+-- There's a bug in package handling that means none of the packages are added
+-- to lua's search path until "slightly too late". Workaround this by moving
+-- affected stuff to happen in the next tick. See
+--   https://github.com/neovim/neovim/issues/12577
+--   https://github.com/neovim/neovim/issues/11409
+vim.schedule(function()
+  require'nvim-treesitter.configs'.setup {
+    ensure_installed = "maintained",
+    highlight = { enable = true },
+  }
+end)
