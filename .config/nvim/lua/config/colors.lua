2d693e2618b0687b0d23fba57d67dd0e2c1d311a
Kamal Marhubi <kamal@marhubi.com>
2020-11-02 00:10:44 -0500

    Switch to vim-which-key for leader mappings

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -8,6 +8,8 @@
 
 vim.cmd([[highlight Comment gui=bolditalic guifg=#444466]])
 vim.cmd([[highlight String gui=italic]])
+vim.cmd([[highlight WhichKeyGroup gui=bold]])
+vim.cmd([[highlight PMenu guibg=#cccccc]])
 
 -- Treesitter highlight groups are defined here:
 --   https://github.com/nvim-treesitter/nvim-treesitter/blob/9327b113c2360d503ab183718c57e3e1b0819887/lua/nvim-treesitter/highlight.lua
--- .config/nvim/lua/config/map.lua
+++ .config/nvim/lua/config/map.lua
@@ -1,11 +1,22 @@
+vim.g.which_key_map = {
+  v = {
+    name = '+vim',
+    r = { [[luaeval("require('config.util').reload()")]], 'reload config' },
+    f = { [[luaeval("require('telescope.builtin').find_files({ cwd = '~/.config/nvim/' })")]], 'find file' },
+    m = { [[:e ~/.config/nvim/lua/config/map.lua]], 'edit mappings' },
+    c = { [[:e ~/.config/nvim/lua/config/colors.lua]], 'edit colors' },
+  },
+  f = { [[luaeval("require('telescope.builtin').git_files{}")]], 'git files' },
+  n = {
+    name = '+navigation',
+    f = { [[luaeval("require('telescope.builtin').git_files{}")]], 'git files' },
+  },
+}
+
 vim.api.nvim_set_keymap('', '<space>', '<leader>', { silent = true })
+vim.api.nvim_set_keymap('', '<leader>', [[<cmd>WhichKey '<space>'<cr>]], { silent = true, noremap = true })
 
--- <leader>v: vim stuff
--- reload
-vim.api.nvim_set_keymap('n', '<leader>vr', [[<cmd>lua require('config.util').reload()<CR>]], { noremap = true })
--- open files
-vim.api.nvim_set_keymap('n', '<leader>vf', [[<cmd>lua require('telescope.builtin').find_files{ cwd = "~/.config/nvim/" }<CR>]], { noremap = true })
-
--- <leader>n: navigation stuff
--- open files
-vim.api.nvim_set_keymap('n', '<leader>nf', [[<cmd>lua require('telescope.builtin').git_files{}<CR>]], { noremap = true })
+-- TODO: Not sure why this is necessary :/
+vim.schedule(function()
+  vim.fn['which_key#register']('<space>', 'g:which_key_map')
+end)
