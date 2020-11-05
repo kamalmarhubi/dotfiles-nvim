3ba4ed3be3cb30ed6f1b61bad21169e22d90109b
Kamal Marhubi <kamal@marhubi.com>
2020-11-05 16:51:32 -0500

    Set ignorecase and smartcase

--- .config/nvim/lua/config/opt.lua
+++ .config/nvim/lua/config/opt.lua
@@ -1,5 +1,9 @@
 vim.o.hidden = true
 
+-- Less annoying search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
 -- Use treesitter for folding.
 vim.o.foldmethod = 'expr'
 vim.o.foldexpr = 'foldexpr=nvim_treesitter#foldexpr()'
