a57333a8e4ea078a001639c12e9ab359b8a236ae
Kamal Marhubi <kamal@marhubi.com>
2020-12-06 23:07:19 -0500

    git subrepo commit --force .local/share/nvim/site/pack/lsp/start/nvim-lspconfig
    subrepo:
      subdir:   ".local/share/nvim/site/pack/lsp/start/nvim-lspconfig"
      merged:   "76f1162"
    upstream:
      origin:   "https://github.com/neovim/nvim-lspconfig.git"
      branch:   "master"
      commit:   "76f1162"
    git-subrepo:
      version:  "0.4.0"
      origin:   "???"
      commit:   "???"

--- .local/share/nvim/site/pack/lsp/start/nvim-lspconfig/lua/lspconfig/pyright.lua
+++ .local/share/nvim/site/pack/lsp/start/nvim-lspconfig/lua/lspconfig/pyright.lua
@@ -15,16 +15,13 @@ configs[server_name] = {
     filetypes = {"python"};
     root_dir = util.root_pattern(".git", "setup.py",  "setup.cfg", "pyproject.toml", "requirements.txt");
     settings = {
-      analysis = { autoSearchPaths= true; };
-      pyright = { useLibraryCodeForTypes = true; };
+      python = {
+        analysis = {
+          autoSearchPaths = true;
+          useLibraryCodeForTypes = true;
+        };
+      };
     };
-    -- The following before_init function can be removed once https://github.com/neovim/neovim/pull/12638 is merged
-    before_init = function(initialize_params)
-            initialize_params['workspaceFolders'] = {{
-                name = 'workspace',
-                uri = initialize_params['rootUri']
-            }}
-    end
    };
   docs = {
     description = [[
