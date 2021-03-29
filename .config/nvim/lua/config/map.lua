009f268476a13b54f250f1eab46c3862dee0f061
Kamal Marhubi <kamal@marhubi.com>
2021-03-29 02:07:46 -0400

    mappings: Switch to nvim-whichkey-setup.lua

--- .config/nvim/lua/config/map.lua
+++ .config/nvim/lua/config/map.lua
@@ -1,4 +1,6 @@
-vim.api.nvim_set_keymap('', '<space>', '<leader>', { silent = true })
+local wk = require('whichkey_setup')
+
+vim.g.mapleader = ' '
 vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true, silent = true })
 
 -- Clear search highlight
@@ -6,38 +8,35 @@ vim.api.nvim_set_keymap('n', '<esc>', '<cmd>nohlsearch<cr>', { noremap = true, s
 vim.api.nvim_set_keymap('n', '<leader><leader>', '<cmd>nohlsearch<cr>', { noremap = true, silent = true })
 
 -- Leader mappings via vim-which-key
-vim.api.nvim_set_keymap('', '<leader>', [[<cmd>WhichKey '<space>'<cr>]], { silent = true, noremap = true })
-vim.g.which_key_map = {
+wk.register_keymap('leader', {
   v = {
     name = '+vim',
-    r = { [[luaeval("require('config.util').reload()")]], 'reload config' },
-    f = { [[luaeval("require('telescope.builtin').find_files({ cwd = '~/.config/nvim/' })")]], 'find file' },
-    m = { [[:e ~/.config/nvim/lua/config/map.lua]], 'edit mappings' },
-    c = { [[:e ~/.config/nvim/lua/config/colors.lua]], 'edit colors' },
-    i = { [[:e ~/.config/nvim/lua/config/init.lua]], 'edit init' },
-    R = { [[:e ~/.config/nvim/README.md]], 'edit readme' },
-    t = { [[:e ~/.config/nvim/TODO.md]], 'edit todo' },
-    o = { [[:e ~/.config/nvim/lua/config/opt.lua]], 'edit options' },
-    T = { [[luaeval("require('telescope.builtin').builtin{}")]], 'telescope builtins' },
+    r = { ":lua require('config.util').reload()<CR>", 'reload config' },
+    f = {
+      ":lua require('telescope.builtin').find_files({ cwd = '~/.config/nvim/' })<CR>",
+      'find file',
+    },
+    m = { ":e ~/.config/nvim/lua/config/map.lua<CR>", 'edit mappings' },
+    c = { ":e ~/.config/nvim/lua/config/colors.lua<CR>", 'edit colors' },
+    i = { ":e ~/.config/nvim/lua/config/init.lua<CR>", 'edit init' },
+    R = { ":e ~/.config/nvim/README.md<CR>", 'edit readme' },
+    t = { ":e ~/.config/nvim/TODO.md<CR>", 'edit todo' },
+    o = { ":e ~/.config/nvim/lua/config/opt.lua<CR>", 'edit options' },
+    T = { ":lua require('telescope.builtin').builtin{}<CR>", 'telescope builtins' },
   },
-  f = { [[luaeval("require('telescope.builtin').git_files{}")]], 'git files' },
+  f = { ":lua require('telescope.builtin').git_files{}<CR>", 'git files' },
   l = {
     name = '+lsp',
-    r = { [[luaeval("require('telescope.builtin').lsp_references()")]], 'references' },
-    d = { [[luaeval("vim.lsp.buf.definition()")]], 'references' },
+    r = { ":lua require('telescope.builtin').lsp_references()<CR>", 'references' },
+    d = { ":lua vim.lsp.buf.definition()<CR>", 'references' },
   },
   g = {
     name = '+git',
-    m = { [[<Plug>(git-messenger)]], 'message at line' },
+    m = { "<Plug>(git-messenger)", 'message at line' },
   },
-  b = { [[luaeval("require('telescope.builtin').buffers{}")]], 'buffers' },
+  b = { ":lua require('telescope.builtin').buffers{}<CR>", 'buffers' },
   n = {
     name = '+navigation',
-    f = { [[luaeval("require('telescope.builtin').git_files{}")]], 'git files' },
+    f = { ":lua require('telescope.builtin').git_files{}<CR>", 'git files' },
   },
-}
-
--- Tell git-messenger not to map <Leader>gm; we'll do it with vim-which-key.
-vim.g.git_messenger_no_default_mappings = true
-
-vim.fn['which_key#register']('<space>', 'g:which_key_map')
+})
