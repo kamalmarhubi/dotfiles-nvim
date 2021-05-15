5e2b7c1c2f27d1a4dfd90e1cf8c91dd45b8a6957
Kamal Marhubi <kamal@marhubi.com>
2021-05-15 18:17:39 -0400

    git subrepo pull .local/share/nvim/site/pack/base/start/nvim-treesitter-textobjects
    subrepo:
      subdir: ".local/share/nvim/site/pack/base/start/nvim-treesitter-textobjects"
      merged: "4f1ace5"
    upstream:
      origin: "https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git"
      branch: "master"
      commit: "4f1ace5"

--- .local/share/nvim/site/pack/base/start/nvim-treesitter-textobjects/queries/julia/textobjects.scm
+++ .local/share/nvim/site/pack/base/start/nvim-treesitter-textobjects/queries/julia/textobjects.scm
@@ -1,6 +1,7 @@
-(compound_expression) @block.outer    ;; begin blocks
+(compound_expression) @block.outer    ; begin blocks
 (let_statement)       @block.outer
 
+(struct_definition) @class.outer    ; not classes, but close enough
 (call_expression) @call.outer
 
 (comment) @comment.outer
@@ -8,9 +9,13 @@
 (if_statement) @conditional.outer
 
 (function_definition) @function.outer
-(function_expression) @function.outer    ;; lambdas
+(assignment_expression
+  (call_expression) (_)) @function.outer    ; math functions
+(function_expression)    @function.outer    ; lambdas
+(macro_definition)       @function.outer
 
 (for_statement)    @loop.outer
 (while_statement)  @loop.outer
 
 (argument_list) @parameter.outer
+(parameter_list) @parameter.outer
